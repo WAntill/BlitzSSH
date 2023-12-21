caddb31ad68d80f123bd6a8b6de972533054442c
William Antill <william.antill1999@outlook.com>
2023-12-21 15:20:49 +0000

    quick name change

--- BlitzSSH.py
+++ BlitzSSH.py
@@ -1,20 +1,20 @@
-import BlitzSSH_Improved
+import BlitzSSH_Utils
 
 def main():
     try:
-        BlitzSSH_Improved.make_IP_List()
+        BlitzSSH_Utils.make_IP_List()
     except:
         print("Failed to set IPs")
     try:
-        BlitzSSH_Improved.get_Passwords()
+        BlitzSSH_Utils.get_Passwords()
     except:
         print("Failed to set Passwords")
     try:
-        BlitzSSH_Improved.get_Users()
+        BlitzSSH_Utils.get_Users()
     except:
         print("Failed to set Users")  
     try:
-        BlitzSSH_Improved.run_Attack()
+        BlitzSSH_Utils.run_Attack()
     except:
         print("Failed to run attack")
 
