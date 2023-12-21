dd3f791ed6cb2a1ccfbf65455b908726c5cbeead
William Antill <william.antill1999@outlook.com>
2023-12-21 15:11:36 +0000

    Added error handing

--- BlitzSSH.py
+++ BlitzSSH.py
@@ -1,10 +1,22 @@
 import BlitzSSH_Improved
 
 def main():
-    BlitzSSH_Improved.make_IP_List()
-    BlitzSSH_Improved.get_Passwords()
-    BlitzSSH_Improved.get_Users()
-    BlitzSSH_Improved.run_Attack()
+    try:
+        BlitzSSH_Improved.make_IP_List()
+    except:
+        print("Failed to set IPs")
+    try:
+        BlitzSSH_Improved.get_Passwords()
+    except:
+        print("Failed to set Passwords")
+    try:
+        BlitzSSH_Improved.get_Users()
+    except:
+        print("Failed to set Users")  
+    try:
+        BlitzSSH_Improved.run_Attack()
+    except:
+        print("Failed to run attack")
 
 if __name__ == "__main__":
     main()
